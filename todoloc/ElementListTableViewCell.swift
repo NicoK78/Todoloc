360d66a373a26570cabc6687708a8f70e4afe4f4
Unknown <viadenouselom@gmail.com>
2018-01-19 11:44:35 +0100

    Ptit oublie

---
+++ todoloc/ElementListTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ElementListTableViewCell.swift
+//  todoloc
+//
+//  Created by Selom Viadenou on 17/01/2018.
+//  Copyright © 2018 Nico. All rights reserved.
+//
+
+import UIKit
+
+class ElementListTableViewCell: UITableViewCell {
+    @IBOutlet var label: UILabel!
+    
+    @IBOutlet var labelSousTitre: UILabel!
+    
+}
